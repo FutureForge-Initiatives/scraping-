2123f533d504ce1e6ae82baeb11c14a475a22fb6
Muhammad Zuhrul Umam <muhammad.zuhrul@efishery.com>
2024-06-06 22:20:37 +0700

    traveloka test

--- Scraping-try-Traveloka.py
+++ Scraping-try-Traveloka.py
@@ -1,16 +1,54 @@
-from bs4 import BeautifulSoup
-import requests
+from playwright.sync_api import sync_playwright
+import time
 
-url = "https://www.traveloka.com/id-id"
-page = requests.get(url)
-soup = BeautifulSoup(page.content, 'html.parser')
+def main():
 
-data = []
-agains = soup.findAll('//*[@id="__next"]/div[1]/div[2]/div/div[2]/div')
+    destination = [
+        "SOLO"
+    ]
 
-for again in agains:
-    nama = again.find('//*[@id="__next"]/div[1]/div[2]/div/div[2]/div/div/a[*]/div').text
+    with sync_playwright() as p:
 
-    data.append(nama)
+        browser = p.chromium.launch(headless=False)
+        page = browser.new_page()
 
-print(data)
+        page_url = 'https://www.traveloka.com/id-id/kereta-api'
+        page.goto(page_url)
+
+        asal = page.get_by_placeholder("Asal")
+        asal.wait_for()
+
+        for dest in destination:
+            asal.click()
+            asal.fill(dest)
+            asalText = page.get_by_text("SLO - Solo Balapan - Solo", exact=True)
+            asalText.click()
+
+            tujuan = page.get_by_placeholder("Tujuan")
+            tujuan.wait_for()
+            tujuan.click()
+            tujuan.fill("Madiun")
+            tujuanText = page.get_by_text("MN - Madiun - Madiun", exact=True)
+            tujuanText.click()
+
+            submitButton = page.get_by_test_id("train-desktop-search-form-cta")
+            submitButton.click()
+
+            listTicketEl = page.get_by_test_id("train-desktop-search-result-list-departure")
+            listTicketEl.wait_for()
+
+            listTicket = page.get_by_test_id("train-desktop-search-result-list-departure-item").all()
+
+            for ticket in listTicket:
+                trainName = ticket.locator("h3").first.inner_text()
+                print(trainName)
+                trainPrice = ticket.locator("h2").first.inner_text()
+                print(trainPrice)
+
+
+        time.sleep(10)
+
+        browser.close()
+
+if __name__ == '__main__':
+    main()
